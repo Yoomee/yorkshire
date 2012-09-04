2bfce743822d1d5a5ad03463017aea0fda829332
Ian Mooney <ian@yoomee.com>
2012-09-04 18:16:21 +0100

    Precompile simulator.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Yorkshire::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( mobile.css mobile.js simulator.css )
+  config.assets.precompile += %w( mobile.css mobile.js simulator.css simulator.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
