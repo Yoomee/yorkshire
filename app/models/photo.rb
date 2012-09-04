0e17b961615e5d3f13201b5b02753831c90dc2df
Ian Mooney <ian@yoomee.com>
2012-09-04 18:51:22 +0100

    Click to view next photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,8 @@ class Photo < ActiveRecord::Base
   validates :image, :presence => true
   validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :message => "must be an image"
   
+  def next_photo
+    Photo.where(['id > ?', id]).first
+  end
+  
 end
