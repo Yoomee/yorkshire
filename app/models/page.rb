e2010d0a30b18e98053aabf1dd1466cf45327fb7
Rich Wells <rich@yoomee.com>
2012-10-12 17:16:01 +0100

    Added more university header options for page/edit

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,7 +11,7 @@ class Page < ActiveRecord::Base
     end
     
     def app_header_image_url(num)
-      "/assets/header_#{num || 11}.jpg"
+      "/assets/header_#{num || 23}.jpg"
     end
 
     def view_names
