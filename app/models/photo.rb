2694c00abf79136490ca7a92da6a93549e5fd8d1
Matt Atkins <matt@yoomee.com>
2012-12-21 17:37:40 +0000

    Photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,14 @@ class Photo < ActiveRecord::Base
   validates :image, :presence => true
   validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :message => "must be an image"
   
+  def caption
+    if image_uid =~ /_W2Y_/
+      "#{read_attribute(:caption)} - With thanks to Welcome to Yorkshire for allowing us to use their image"
+    else
+      read_attribute(:caption)
+    end
+  end
+  
   def next_photo
     Photo.where(['id > ?', id]).first
   end
