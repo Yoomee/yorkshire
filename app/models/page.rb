33335d2fd4f7045c92ca4e9f23ce564f48098c13
Rich Wells <rich@yoomee.com>
2013-01-07 17:13:31 +0000

    Added another image

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,7 +11,7 @@ class Page < ActiveRecord::Base
     end
     
     def app_header_image_url(num)
-      "/assets/header_#{num || 45}.jpg"
+      "/assets/header_#{num || 46}.jpg"
     end
 
     def view_names
